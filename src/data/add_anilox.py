07ba1c7e5910e8d1b9e2156de3809c5893fa5ce1
Marquel Rogers <69293041+msrogers2015@users.noreply.github.com>
2024-11-05 09:50:33 -0600

    Update add_anilox.py
    Changed the port as the current port was for my specific pc.

--- src/data/add_anilox.py
+++ src/data/add_anilox.py
@@ -1,7 +1,7 @@
 import requests
 from random import choice, randint
 
-url = 'http://127.0.0.1:5001/add_anilox'
+url = 'http://127.0.0.1:5000/add_anilox'
 roller = randint(10000,99999)
 data_list = ['200-11.0', '360-8.0', '400-6.5', '440-6.0', '550-5.0', '600-5.0', '800-3.5','800-2.2', '900-2.2', '1000-2.0']
 lpi, bcm = choice(data_list).split('-')
